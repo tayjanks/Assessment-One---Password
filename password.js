bcf5da05e17aeb88dc98ab485cd56fdfc1ce60c1
tayjanks <taylorjenkins801@gmail.com>
2022-08-27 13:39:27 -0600

    trying ascii script

--- password.js
+++ password.js
@@ -1,11 +1,18 @@
-let password = "aA223343243432"
+let password = "aA223343243432!"
 
 function containsNumber (str){return /\d/.test(str)} 
 function containsLowerLetter (str){return /[a-z]/.test(str)}
 function containsUpperLetter (str){return /[A-Z]/.test(str)}
 function contiansSpecialChar(str){const specialChar =/[!@#$%^&*]/; return specialChar.test(str)}
 
-if (containsNumber(password)===false){console.log ("Your password needs at least one number")}
+if (containsNumber(password)===false){console.log ("Your password needs at least one number\n"+"
+___________                                    .__      ._.
+\__    ___/______ ___.__. _____     _________  |__| ____| |
+  |    |  \_  __ <   |  | \__  \   / ___\__  \ |  |/    \ |
+  |    |   |  | \/\___  |  / __ \_/ /_/  > __ \|  |   |  \|
+  |____|   |__|   / ____| (____  /\___  (____  /__|___|  /_
+                  \/           \//_____/     \/        \/\/
+")}
 else if (containsLowerLetter(password)===false){console.log("Your password needs at least one lower-case letter")}
 else if (containsUpperLetter(password)===false){console.log("Your password needs at least one upper-case letter")}
 else if (contiansSpecialChar(password)===false){console.log("Your password needs at least one special character")}
