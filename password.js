6b4eb2f33bfcddc56155c18327ec7b8ecda4fe86
tayjanks <taylorjenkins801@gmail.com>
2022-08-27 13:29:14 -0600

    trying to figure out special characters

--- password.js
+++ password.js
@@ -1,10 +1,14 @@
-let password = "A2!!!"
+let password = "aA2!!!!!!!!!!"
 nums = [0,1,2,3,4,5,6,7,8,9]
 function containsNumber (str){return /\d/.test(str)} 
-function containsLetter (str){return /[a-z]/.test(str)}
+function containsLowerLetter (str){return /[a-z]/.test(str)}
+function containsUpperLetter (str){return /[A-Z]/.test(str)}
+function contiansSpecialChar(str){const specialChar =["!@#$%^&*()_+\-=\[\]{};:\\|,.<>\/?~/"]; return specialChar.test(str)}
 
 if (containsNumber(password)===false){console.log ("Need to add a number")}
-else if (containsLetter(password)===false){console.log("Need to add a lower-case letter")}
+else if (containsLowerLetter(password)===false){console.log("Need to add a lower-case letter")}
+else if (containsUpperLetter(password)===false){console.log("Needs at least one upper-case letter")}
+else if (contiansSpecialChar(password)===false){console.log("Needs at least one special character")}
 else if (password.length < 10 || password.length >20)
 {console.log ("Password must be between 10 and 20 characters")}
 else [console.log("Acceptable")]
